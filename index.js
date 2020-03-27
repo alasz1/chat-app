12166eb09201eaa1e2ede6e19e8c40755995d728
alasz <antti.laszlo@gmail.com>
2020-03-27 10:35:50 +0200

    modified app.listen

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ io.on("connection", function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
+
+app.listen(process.env.PORT || 3000);
